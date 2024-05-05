a6f49fdee6838c781dbff1782dc3b2352265d776
Martim Bello <martimbello@live.com.pt>
2024-05-06 00:42:51 +0100

    Dockerized the API

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bashset -e 
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /ParkMe/tmp/pids/server.pid 
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
+view raw
